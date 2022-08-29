d909e8eb4e6dc19932b82556403ef2f65068203a
GiannaDiGregorio <giannaangel21@gmail.com>
2022-08-28 23:36:13 -0400

    fixed class names

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -2,7 +2,7 @@ function Box(props) {
   return (
     <a href={props.link} target="_blank" rel="noreferrer">
       <div className={props.theme ? "boxdivdark" : "boxdiv"}>
-        <h2 className={props.theme ? "darktext" : null}>{props.title}</h2>
+        <h2 className={props.theme ? "darktextbox" : null}>{props.title}</h2>
       </div>
     </a>
   );
