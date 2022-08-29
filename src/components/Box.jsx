f1a67439b0f570652297e7641b38e302eb0cc7bb
Gianna DiGregorio <gianna@Giannas-MacBook-Air-4.local>
2022-08-28 22:26:47 -0400

    fixed non ref warning

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,6 +1,6 @@
 function Box(props) {
   return (
-    <a href={props.link} target="_blank">
+    <a href={props.link} target="_blank" rel="noreferrer">
       <div className={props.theme ? "boxdivdark" : "boxdiv"}>
         <h2 className={props.theme ? "dark" : null}>{props.title}</h2>
       </div>
